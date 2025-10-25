213a307767a3302422435f7b89976a4f49de4844
ZeiKei9 <artem.kovalv@internet.ru>
2025-10-25 19:30:45 +0300

    categories model

--- device_management/devices/admin.py
+++ device_management/devices/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at']
+    search_fields = ['name', 'description']
+    list_filter = ['created_at']
