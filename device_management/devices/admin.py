a688adab2b26f7b6c7a816d4e550482ae1a81aea
ZeiKei9 <artem.kovalv@internet.ru>
2025-10-26 18:03:54 +0300

    brand model

--- device_management/devices/admin.py
+++ device_management/devices/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Brand
 
 
 @admin.register(Category)
@@ -7,3 +7,10 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'created_at', 'updated_at']
     search_fields = ['name', 'description']
     list_filter = ['created_at']
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country', 'website', 'created_at']
+    search_fields = ['name', 'country']
+    list_filter = ['country', 'created_at']
--- device_management/devices/models.py
+++ device_management/devices/models.py
@@ -14,3 +14,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
