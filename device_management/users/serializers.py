858edcd4ac3643c7f50055923662dded27500d70
ZeiKei9 <artem.kovalv@internet.ru>
2025-11-10 22:24:13 +0300

    registration and auth serializers

--- device_management/users/serializers.py
+++ device_management/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Role, UserProfile
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 
 class RoleSerializer(serializers.ModelSerializer):
@@ -47,4 +48,58 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "profile"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "profile",
+            "date_joined",
+        ]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+    phone = serializers.CharField(required=False, allow_blank=True)
+    department = serializers.CharField(required=False, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "password",
+            "password2",
+            "email",
+            "first_name",
+            "last_name",
+            "phone",
+            "department",
+        ]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        phone = validated_data.pop("phone", None)
+        department = validated_data.pop("department", None)
+
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
+            password=validated_data["password"],
+        )
+
+        UserProfile.objects.create(user=user, phone=phone, department=department)
+
+        return user
